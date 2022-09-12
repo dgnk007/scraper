899ccc9a398071fd0b3d79e9a5c9d27fbf067756
dgnk007 <dgnk007@gmail.com>
2022-09-12 15:57:27 +0530

    optimised

--- scraper/load.py
+++ scraper/load.py
@@ -1,12 +1,11 @@
 import multi_scrap,psycopg2
-items = ['laptop','t shirt','smart phone',]
-data = multi_scrap.flipkartPage('laptop',1,21)
-postgres_insert_query = """ INSERT INTO importflipkart (id, title, link,price) VALUES (%s,%s,%s,%s)"""
+items = ['laptop','t shirt','smart phone']
+postgres_insert_query = """ INSERT INTO importflipkart (id,title,link,price,keyword) VALUES (%s,%s,%s,%s,%s)"""
 conn = psycopg2.connect(database="scraper", user='postgres', password='Axims0ft123', host='localhost', port= '5432')
 cursor = conn.cursor()
-for i in data:
-    cursor.execute(postgres_insert_query, (i["id"],i["title"], i["link"],i["price"]))
+for item in items:
+    data = multi_scrap.flipkartPage(item,1,21)
+    for i in data:
+        cursor.execute(postgres_insert_query, (i["id"],i["title"], i["link"],i["price"],item))
 conn.commit()
 conn.close()
-
-
