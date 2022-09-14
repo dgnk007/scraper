fa4b8854309fd0baf919ef6950f051d8c0931fb6
dgnk007 <dgnk007@gmail.com>
2022-09-14 19:36:17 +0530

    data issue fixed

--- scraper/load.py
+++ scraper/load.py
@@ -1,5 +1,5 @@
 import multi_scrap,psycopg2
-items = ['laptop','t shirt','smart phone']
+items = ['synology']
 postgres_insert_query = """ INSERT INTO importflipkart (id,title,link,price,keyword) VALUES (%s,%s,%s,%s,%s)"""
 conn = psycopg2.connect(database="scraper", user='postgres', password='Axims0ft123', host='localhost', port= '5432')
 cursor = conn.cursor()
--- scraper/multi_scrap.py
+++ scraper/multi_scrap.py
@@ -1,7 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-
-
 # for page in pages:
 #     response = requests.get("https://www.amazon.in/s?k=laptop&page={}".format(page))
 #     soup = BeautifulSoup(response.text, 'html.parser')
@@ -20,12 +18,18 @@ def flipkartPage(search,start_,end_):
     pages =list(range(start_,end_))
     content =[]
     for page in pages:
-        response = requests.get("https://www.flipkart.com/search?q={}&page={}".format(search,page))
-        con = BeautifulSoup(response.text, 'html.parser').find_all('div',class_='_2kHMtA')
+        try:
+            response = requests.get("https://www.flipkart.com/search?q={}&page={}".format(search,page))
+            con = BeautifulSoup(response.text, 'html.parser').find_all('div',class_='_2kHMtA')
+        except:
+            break
         for i in range(len(con)):
             json_data = {}
             json_data["title"] = con[i].find('div',class_='_4rR01T').text.encode('utf-8')
-            json_data["price"] = con[i].find('div',class_='_30jeq3').text[1:].encode('utf-8').replace(',','')
+            try:
+                json_data["price"] = con[i].find('div',class_='_30jeq3').text[1:].encode('utf-8').replace(',','')
+            except:
+                json_data["price"] = None
             json_data["link"] = 'www.flipkart.com' + con[i].find('a',class_='_1fQZEK').get("href").encode('utf-8')
             json_data['id'] = json_data['link'].split('/')[-1].split('?')[0]
             content.append(json_data)
